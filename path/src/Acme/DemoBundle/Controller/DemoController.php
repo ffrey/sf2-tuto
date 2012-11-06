ad2dabb038e47c65fa34e3f8f0f45c0a8099de3b
ffrey <ffrey.web@gmail.com>
2012-11-06 16:27:54 +0100

    chapter 3 ended

--- path/src/Acme/DemoBundle/Controller/DemoController.php
+++ path/src/Acme/DemoBundle/Controller/DemoController.php
@@ -12,58 +12,83 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DemoController extends Controller
 {
-    /**
-     * @Route("/", name="_demo")
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return array();
-    }
+	/**
+	 * @Route("/", name="_demo")
+	 * @Template()
+	 */
+	public function indexAction()
+	{
+		return array();
+	}
 
-    /**
-     * @Route("/hello/{name}.{_format}", defaults={"_format"="html"}, name="_demo_hello",
-     	requirements={"_format"="html|xml|json"})
-     * @Template()
-     */
-    public function helloAction($name)
-    {
-        return array('name' => $name);
-    }
+	/**
+	 * @Route("/hello/{name}.{_format}", defaults={"_format"="html"}, name="_demo_hello",
+	 requirements={"_format"="html|xml|json"})
+	 * @Template()
+	 */
+	public function helloAction($name)
+	{
+		$db = false; $origin = __CLASS__.'::'.__METHOD__;
+		if (true) {
+			$name = 'redirectMan';
+			$url = $this->generateUrl('_demo_fancy', array('name' => $name) );
+			if ($db) {
+				var_dump($origin
+						, 'url', $url
+						, 'stop !'); exit;
+			}
+			$session = $this->getRequest()->getSession();
+			$session->getFlashBag()->add('notice', 'Congratulations, your redirect succeeded!');
+			return $this->redirect($url);
+		} else if (true) {
+			$name = 'forwardMan';
+			$response = $this->forward('AcmeDemoBundle:Demo:fancy', array('name' => $name, 'color' =>
+					'green'));
+			return $response;
+		} else {
+			return array('name' => $name);
+		}
+	}
 
-    /**
-     * @Route("/contact", name="_demo_contact")
-     * @Template()
-     */
-    public function contactAction()
-    {
-        $form = $this->get('form.factory')->create(new ContactType());
+	/**
+	 * @Route("/contact", name="_demo_contact")
+	 * @Template()
+	 */
+	public function contactAction()
+	{
+		$form = $this->get('form.factory')->create(new ContactType());
 
-        $request = $this->get('request');
-        if ('POST' == $request->getMethod()) {
-            $form->bindRequest($request);
-            if ($form->isValid()) {
-                $mailer = $this->get('mailer');
-                // .. setup a message and send it
-                // http://symfony.com/doc/current/cookbook/email.html
+		$request = $this->get('request');
+		if ('POST' == $request->getMethod()) {
+			$form->bindRequest($request);
+			if ($form->isValid()) {
+				$mailer = $this->get('mailer');
+				// .. setup a message and send it
+				// http://symfony.com/doc/current/cookbook/email.html
 
-                $this->get('session')->setFlash('notice', 'Message sent!');
+				$this->get('session')->setFlash('notice', 'Message sent!');
 
-                return new RedirectResponse($this->generateUrl('_demo'));
-            }
-        }
+				return new RedirectResponse($this->generateUrl('_demo'));
+			}
+		}
 
-        return array('form' => $form->createView());
-    }
-    
-    /**
-     * @Route("/fancy", name="_demo_fancy")
-     */
-    public function fancyAction($name = null)
-    {
-    	$name = $this->getRequest()->query->get('name');
-    	$name = ($name)? $name : 'bonjour';
-		return $this->render('AcmeDemoBundle:Demo:fancy.html.twig', 
-			array('name' => $name,)
-		);    }
+		return array('form' => $form->createView());
+	}
+
+	/**
+	 * @Route("/fancy", name="_demo_fancy")
+	 */
+	public function fancyAction($name = null)
+	{
+		$db = false; $origin = __CLASS__.'::'.__METHOD__;
+		if ($db) {
+			var_dump($origin
+					, 'stop !'); exit;
+		}
+		$name = $this->getRequest()->query->get('name');
+		$name = ($name)? $name : 'bonjour';
+		return $this->render('AcmeDemoBundle:Demo:fancy.html.twig',
+				array('name' => $name,)
+		);
+	}
 }
--- path/src/Acme/DemoBundle/Controller/SecuredController.php
+++ path/src/Acme/DemoBundle/Controller/SecuredController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 
 /**
@@ -50,6 +51,7 @@ class SecuredController extends Controller
     /**
      * @Route("/hello", defaults={"name"="World"}),
      * @Route("/hello/{name}", name="_demo_secured_hello")
+     * @Secure(roles="ROLE_ADMIN")
      * @Template()
      */
     public function helloAction($name)
@@ -60,6 +62,7 @@ class SecuredController extends Controller
     /**
      * @Route("/hello/admin/{name}", name="_demo_secured_hello_admin")
      * @Secure(roles="ROLE_ADMIN")
+     * @Cache(maxage="86400")
      * @Template()
      */
     public function helloadminAction($name)
