7f1ba3a5951cce3a4824d275fa3fc0340cefe597
ffrey <ffrey.web@gmail.com>
2012-11-09 17:53:22 +0100

    suite

--- path/app/AppKernel.php
+++ path/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         	new Acme\BlogBundle\AcmeBlogBundle(),
+            new Acme\HelloBundle\AcmeHelloBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
