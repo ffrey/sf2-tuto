9a0b8324185986dddbaf9d631054d3005d246a14
ffrey@home <ffrey.web@gmail.com>
2012-11-11 23:00:29 +0100

    progress

--- path/src/Acme/HelloBundle/Controller/HelloController.php
+++ path/src/Acme/HelloBundle/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace Acme\HelloBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HelloController extends Controller
@@ -9,5 +10,11 @@ class HelloController extends Controller
     public function indexAction($name)
     {
         return $this->render('AcmeHelloBundle:Hello:index.html.twig', array('name' => $name));
+    }
+    
+    public function anotherAction($id, $_controller, $_route)
+    {
+    	$msg = sprintf('<html><body>The id is %s (%s / %s)</body></html>', $id, $_controller, $_route);
+    	return new Response($msg);
     }
 }
